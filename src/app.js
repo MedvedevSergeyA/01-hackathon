4b58ec6859f19f616e94855c7767560e06b43ccf
Dezolder <evgeniy.zhukov.2012@gmail.com>
2022-06-26 00:02:27 +0300

    Implemented background module

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import './styles.css'
 import {ContextMenu} from './menu';
+import {BackgroundModule} from './modules/background.module'
 
 const menu = new ContextMenu('.menu');
 menu.open()
+menu.add(new BackgroundModule('backround', 'Сменить фон'));
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,15 @@
-import {Module} from '../core/module'
+import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
+    //     printClass() {
+    // }
 
+    trigger() {
+        // document.styl
+        // console.log('BackgroundModule')
+        const colors = ['#16217b', '#1b297c', '#4a42a8', '#7159bd', '#8861ba', '#00abca', '#a667bd']
+        let n = Math.floor(Math.random() * colors.length)
+        document.body.style.backgroundColor = colors[n]
+
+    }
 }
