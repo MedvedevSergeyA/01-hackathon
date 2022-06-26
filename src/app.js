8b2618c8e2d8d968248825e19d299658ff2e088b
MedvedevSergeyA <99241712+MedvedevSergeyA@users.noreply.github.com>
2022-06-26 18:44:45 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import './styles.css'
 import {ContextMenu} from './menu';
 import {BackgroundModule} from './modules/background.module'
-
-const menu = new ContextMenu('.menu');
-menu.add(new BackgroundModule('background', 'Сменить фон'));
-menu.open()
-
 import {SoundModule} from './modules/random-sound'
 
+const menu = new ContextMenu('.menu');
+
+menu.add(new BackgroundModule('background', 'Сменить фон'));
 menu.add(new SoundModule('sound', 'Включить случайную мелодию'))
+
+menu.open()
