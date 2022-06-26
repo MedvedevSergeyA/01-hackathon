53567f4c572813814309f7ffc5e62f05d66ff9a2
MedvedevSergeyA <99241712+MedvedevSergeyA@users.noreply.github.com>
2022-06-26 17:19:10 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,5 +3,6 @@ import {ContextMenu} from './menu';
 import {BackgroundModule} from './modules/background.module'
 
 const menu = new ContextMenu('.menu');
+menu.add(new BackgroundModule('background', 'Сменить фон'));
 menu.open()
-menu.add(new BackgroundModule('backround', 'Сменить фон'));
+
