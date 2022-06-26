36ea7b9fe3d6723a3876fc8029c38d88f9c582e2
MedvedevSergeyA <99241712+MedvedevSergeyA@users.noreply.github.com>
2022-06-27 01:43:19 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ import {ContextMenu} from './menu';
 import {ClicksModule} from  './modules/clicks.module'
 import {SoundModule} from './modules/random-sound'
 import {BackgroundModule} from './modules/background.module'
+import { ShapeModule } from './modules/shape.module'
+
 
 const menu = new ContextMenu('.menu');
 
-menu.add(new BackgroundModule('background', 'Сменить фон'));
+menu.add(new BackgroundModule('background', 'Сменить фон'))
 menu.add(new SoundModule('sound', 'Включить случайную мелодию'))
 menu.add(new ClicksModule('click', 'Клик!'))
+menu.add(new ShapeModule('ShapeModule', 'Создать случайную фигуру'))
 
 menu.open()
