22293223449f000249512544fb7beb50419231f3
MedvedevSergeyA <99241712+MedvedevSergeyA@users.noreply.github.com>
2022-06-26 17:30:59 +0500

    Update background.module.js

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,9 +1,6 @@
 import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
-    constructor (type, text) {
-        super(type, text)
-    }
 
     trigger() {
         const colors = ['#16217b', '#1b297c', '#4a42a8', '#7159bd', '#8861ba', '#00abca', '#a667bd']
