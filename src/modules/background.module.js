889a352277e50a4dccdcb88f73db4ebbe07eef6b
MedvedevSergeyA <99241712+MedvedevSergeyA@users.noreply.github.com>
2022-06-26 17:21:44 +0500

    Update background.module.js

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,15 +1,14 @@
 import { Module } from '../core/module'
 
 export class BackgroundModule extends Module {
-    //     printClass() {
-    // }
+    constructor (type, text) {
+        super(type, text)
+    }
 
     trigger() {
-        // document.styl
-        // console.log('BackgroundModule')
         const colors = ['#16217b', '#1b297c', '#4a42a8', '#7159bd', '#8861ba', '#00abca', '#a667bd']
+        
         let n = Math.floor(Math.random() * colors.length)
         document.body.style.backgroundColor = colors[n]
-
     }
 }
